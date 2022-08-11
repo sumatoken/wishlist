203516b98e1c6dcb284e77e304e9447ac74f0cd7
Sumato Ken <bermime1@gmail.com>
2022-08-11 11:42:10 +0100

    user can switch modes

--- components/home_pages/HomePage.js
+++ components/home_pages/HomePage.js
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import Address from "../sections/Address";
 import Links from "../sections/Links";
@@ -7,21 +7,39 @@ import Story from "../sections/Story";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useSWR from "swr";
+import PublicHomePage from "./PublicHomePage";
 
 export default function HomePage({ alias }) {
+  const [mode, setMode] = useState(true);
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error } = useSWR(`/api/user/links/${alias}`, fetcher);
   const { data: session } = useSession();
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-
+  if (!mode) return <PublicHomePage alias={alias} />;
   console.log("data", data);
   return (
     <>
       <Header />
       <ToastContainer />
       <div className="w-full flex flex-col gap-4 justify-center align-center items-center">
+        <label
+          htmlFor="default-toggle"
+          className="inline-flex relative items-center cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            value=""
+            id="default-toggle"
+            className="sr-only peer"
+            onChange={() => setMode(!mode)}
+          />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+            Switch modes
+          </span>
+        </label>
         <Story fullname={data.fullname} story={data.story} />
         <Address address={data.address} />
         <Links links={data.links} />
--- components/home_pages/PublicHomePage.js
+++ components/home_pages/PublicHomePage.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import Address from "../sections/Address";
 import Story from "../sections/Story";
 import PublicLinks from "../sections/links/PublicLinks";
 import useSWR from "swr";
 import Loading from "../utils/Loading";
+import { useSession } from "next-auth/react";
+import HomePage from "./HomePage";
 
 export default function PublicHomePage({ alias }) {
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const [mode, setMode] = useState(false);
 
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data: session } = useSession();
   const { data, error } = useSWR(`/api/user/links/${alias}`, fetcher);
   if (error) return <div>failed to load</div>;
   if (!data) return <Loading />;
-
+  if (session?.user.username === alias && mode) {
+    return <HomePage alias={alias} />;
+  }
   return (
     <>
       <Header />
       <div className="flex flex-col gap-4 justify-center align-center items-center">
+        {session?.user.username === alias && (
+          <label
+            htmlFor="default-toggle"
+            className="inline-flex relative items-center cursor-pointer"
+          >
+            <input
+              type="checkbox"
+              value=""
+              id="default-toggle"
+              className="sr-only peer"
+              onChange={() => setMode(!mode)}
+            />
+            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Switch modes
+            </span>
+          </label>
+        )}
         <Story fullname={data.fullname} story={data.story} />
         <Address address={data.address} />
         <PublicLinks links={data.links} />
