115291a3841cbc239e5a7dfdc37ec4753675f610
Mohammed Bermime <bermime1@gmail.com>
2023-08-06 21:12:04 +0100

    Added landing page

--- components/Header.js
+++ components/Header.js
@@ -41,7 +41,7 @@ export default function Header() {
   }; */
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+      <nav className="bg-black border-gray-200 px-4 lg:px-6 py-2.5 ">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <div className="flex items-center">
             <Image
@@ -67,7 +67,7 @@ export default function Header() {
               </Link>
               <button
                 onClick={() => signIn(null, { callbackUrl: `/` })}
-                className="button text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                className="button bg-white text-gray-800 dark:text-black hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
                 Log In
               </button>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -12,8 +10,37 @@ export default function Home() {
         <title>Home Page</title>
       </Head>
       <Header />
-      dd
-      {process.env.customKey}{" "}
+
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          We build Wishtrees for you!
+        </h1>
+        <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+          Here at Tree Linkin we focus on building the best communities for
+          sharing wishlists with people like you from all over the world.
+        </p>
+        <a
+          href="#"
+          className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        >
+          Learn more
+          <svg
+            className="w-3.5 h-3.5 ml-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 10"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 5h12m0 0L9 1m4 4L9 9"
+            />
+          </svg>
+        </a>
+      </div>
     </>
   );
 }
